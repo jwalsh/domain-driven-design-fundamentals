5103dc24ad9b8b0de25e6f0e8407bdb7c74ebd87
Jason Walsh <j@wal.sh>
2021-02-12 06:41:24 -0500

    Scaffold DDD

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace domain_driven_design_fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Domain-Driven Design Fundamentals");
+        }
+    }
+}
